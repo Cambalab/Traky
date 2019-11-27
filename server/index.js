8667fe039df9e8bea8fc126561633cf0c870d619
Jesus Laime <jesus.laime@camba.coop>
2019-11-27 18:26:57 -0300

    change SERVER_PORT to PORT, fix cors error allowing all method

--- server/index.js
+++ server/index.js
@@ -3,20 +3,27 @@ const express = require('express');
 const path = require('path');
 const server = jsonServer.create();
 
-const PORT = process.env.SERVER_PORT || 8000;
-const HOST = process.env.SERVER_HOST || "localhost";
+const PORT = process.env.PORT || 8000;
 const STATIC_FOLDER_PATH = process.env.STATIC_FOLDER_PATH || path.join(__dirname, '../build');
 const STATIC_INDEX_PATH = process.env.STATIC_INDEX_PATH || path.join(__dirname, '../build/index.html');
 const DATA_JSON_PATH = process.env.DATA_JSON_PATH || path.join(__dirname, 'data.json');
 const ROUTES_PATH = process.env.ROUTES_PATH || path.join(__dirname, 'routes.json');
-const routes = require(ROUTES_PATH);
-const router = jsonServer.router(DATA_JSON_PATH);
+
+let routes = {};
+let router;
+try {
+    routes = require(ROUTES_PATH);
+    router = jsonServer.router(DATA_JSON_PATH);
+} catch(e) {
+    console.log(e);
+}
 
 /*
     @TODO: we need to check if it is necessary in staging
 */
 server.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
@@ -28,6 +35,6 @@ server.get('/*', function (req, res) {
     res.sendFile(STATIC_INDEX_PATH);
 });
 
-server.listen(PORT, HOST, () => {
-    console.log(`Server is running on ${HOST}:${PORT}`);
+server.listen(PORT, () => {
+    console.log(`server is running on ${PORT}`);
 });
