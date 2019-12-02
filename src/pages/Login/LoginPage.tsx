4c551fc4beae0a51c95aee126c5ff51d856c3777
leonardo <leonardo@camba.lan>
2019-12-02 16:16:21 -0300

    Add store management for login and separate LoginForm logic to other Component

--- src/pages/Login/LoginPage.tsx
+++ src/pages/Login/LoginPage.tsx
@@ -1,99 +1,46 @@
-import React, { FunctionComponent, useState, CElement } from 'react';
-import {LOGIN_PAGE_TEXTS} from "./constants";
+import React, { FunctionComponent, useContext} from 'react';
 import "./LoginPage.css";
+import { AppContext } from "../../store/Store";
+import { History } from "history";
+import LoginForm from "../../components/LoginForm/LoginForm";
+import { URL_CONFIG } from "../../utils/constants";
+import { loginUser } from "../../utils/api";
 import {
   IonPage,
-  IonContent,
-  IonButton,
-  IonItem,
-  IonLabel,
-  IonInput,
-  IonGrid,
-  IonRow,
-  IonCol,
-  IonImg,
+  IonContent
 } from '@ionic/react';
 
-interface LoginPageProps {
-  history: History,
-  initialUsername: string,
-  initialPassword: string
-}
-
 interface LoginPage {
-    history: History,
-    username: string,
-    password: string
+  history: History
 }
 
-const IMAGE_URL = "https://recursos.camba.coop/uploads/-/system/project/avatar/299/logo-traky2.png?width=40";
+const LoginPage: FunctionComponent<LoginPage> = ({ history }) => {
 
-const LoginPage: FunctionComponent<LoginPageProps> = ({ history,
-  initialUsername = "",
-  initialPassword = "",
-}) => {
-    const [username, setUsername] = useState<string>(initialUsername);
-    const [password, setPassword] = useState<string>(initialPassword);
+  const { state, dispatch } = useContext(AppContext);
 
-    const onChangeValue = (event :any) => {
-      setUsername(event.detail.value);
+  const onClickLogin = async (body: LoginForm) => {
+    console.log(body)
+    const onSuccess = (res: any) => {
+      dispatch({
+        type: 'SET_USER',
+        payload: {id: res.id, name: body.username} // state.user.name
+      })
+      dispatch({
+        type: 'LOGIN',
+        payload: true
+      })
+      history.push(URL_CONFIG.LOGS_LIST.path)
     }
+    await loginUser(body.username, body.password, body, onSuccess) // llamada a api
+  }
 
-    const validateEmail = (event :any) => {
-      const reg = /^(.*[@].*[.com])$/;
-      const isValid = reg.test(username);
-      let element = event.srcElement;
-
-      element.color = isValid ? "dark" : "danger";
-    }
-
-    const logUser = (event :any) => {
-      console.log(event)
-      var a = event.preventDefault();
-      console.log("ASD ", a)
-    }
-
-    return (
-      <IonPage>
-        <IonContent className="toolbar--background ">
-          <IonGrid>
-            <IonRow>
-              <IonCol sizeXs="10" offsetXs="1" sizeMd="4" offsetMd="4">
-                <IonImg className="login-img" src={IMAGE_URL}/>
-                <IonItem className="login-input">
-                  <IonLabel position="floating" >{LOGIN_PAGE_TEXTS.USERNAME}</IonLabel>
-                  <IonInput
-                    color="dark"
-                    name="username"
-                    type="email"
-                    inputmode="email"
-                    pattern="/^(.*[@].*[.com])$/"
-                    value={username}
-                    autofocus={true}
-                    onIonChange={onChangeValue}
-                    onIonBlur={validateEmail}
-                  />
-                </IonItem>
-                <IonItem className="login-input">
-                  <IonLabel position="floating">{LOGIN_PAGE_TEXTS.PASSWORD}</IonLabel>
-                  <IonInput
-                    name="password"
-                    type="password"
-                    value={password}
-                    minlength={6}
-                  />
-                </IonItem>
-                <IonButton
-                  color="dark"
-                  className="login-button"
-                  onClick={e => logUser(e)}
-                >{LOGIN_PAGE_TEXTS.LOGIN_BUTTON}</IonButton>
-              </IonCol>
-            </IonRow>
-          </IonGrid>
-        </IonContent>
-      </IonPage>
-    )
+  return (
+    <IonPage>
+      <IonContent color="primary">
+        <LoginForm onClickLogin={onClickLogin}/>
+      </IonContent>
+    </IonPage>
+  )
 }
 
 export default LoginPage;
