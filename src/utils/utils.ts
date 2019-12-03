c22d465c51122a40163289459fd78c9f9a5bab72
leonardo <leonardo@camba.lan>
2019-12-03 11:55:44 -0300

    Refactored functions for get class name depends to validate into utils.tsx file

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -5,3 +5,31 @@ export function isMobile() {
     return true;
   }
 }
+
+export const isValid = (data: string, dataValidate: boolean): boolean => {
+  return dataValidate || data.length === 0
+}
+
+export const getElementClassNameError = (element: string, elementValidate: boolean): string => {
+  return isValid(element, elementValidate) ? '': 'error'
+}
+
+export const getElementColor = (element: string, elementValidate: boolean): string => {
+  return getElementClassNameError(element, elementValidate) === 'error' ? 'danger': 'dark';
+}
+
+export const getUsernameClassNameError = (username: string, usernameValidate: boolean): string => {
+  return getElementClassNameError(username, usernameValidate)
+}
+
+export const getPasswordClassNameError = (password: string, passwordValidate: boolean): string => {
+  return getElementClassNameError(password, passwordValidate)
+}
+
+export const getPasswordColor = (password: string, passwordValidate: boolean): string => {
+  return getElementColor(password, passwordValidate)
+}
+
+export const getUsernameColor = (username: string, usernameValidate: boolean): string => {
+  return getElementColor(username, usernameValidate)
+}
