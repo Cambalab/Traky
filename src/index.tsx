df8dc46383ce670b0c21b352b2c0517e34178631
Jesus Laime <jesus.laime@camba.coop>
2019-12-06 12:53:24 -0300

    fix import

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import App from "./pages/App/App";
 import { AppContextProvider } from "./store/Store";
 
 ReactDOM.render(
