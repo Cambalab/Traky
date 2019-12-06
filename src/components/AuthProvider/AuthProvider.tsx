ab6a94c14d799d57a276639cb66b6587bdc2d92f
Jesus Laime <jesus.laime@camba.coop>
2019-12-06 12:29:48 -0300

    fix page components using auth provider were not passed the history prop

--- src/components/AuthProvider/AuthProvider.tsx
+++ src/components/AuthProvider/AuthProvider.tsx
@@ -1,19 +1,19 @@
 import React, { useContext } from "react";
 import { Redirect } from "react-router";
 import { AppContext } from "../../store/Store";
-import { URL_CONFIG } from "../../utils/constants";
+import { LOGS_LOGIN_URL_CONFIG } from "../../utils/constants";
 
 interface AuthProviderProps {
   Component: React.FunctionComponent<any>;
 }
 
-const AuthProvider: React.FC<AuthProviderProps> = ({ Component }) => {
+const AuthProvider: React.FC<AuthProviderProps> = ({ Component, ...props }) => {
   const { state } = useContext(AppContext);
 
   return state.isLoged ? (
-    <Component />
+    <Component {...props} />
   ) : (
-    <Redirect to={URL_CONFIG.LOGS_LOGIN.path} />
+    <Redirect to={LOGS_LOGIN_URL_CONFIG.path} />
   );
 };
 
