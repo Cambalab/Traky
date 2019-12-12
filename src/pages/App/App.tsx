cd43992f45aa0d5bbd22b0e8a28890b39d7a573d
leonardo <leonardo@camba.lan>
2019-12-12 10:43:56 -0300

    Added Notification in App Component to show it

--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import { Redirect, Route } from "react-router-dom";
 import { IonApp, IonRouterOutlet, IonSplitPane } from "@ionic/react";
 import { IonReactRouter } from "@ionic/react-router";
+import { list, timer, contact } from "ionicons/icons";
+import { AppPage, ILogs } from "../../utils/declarations";
+import { getCurrentUser, getHours } from "../../utils/api";
 
 /* Core CSS required for Ionic components to work properly */
 import "@ionic/react/css/core.css";
@@ -32,11 +35,66 @@ import EditHourPage from "../../pages/EditHour/EditHourPage";
 /* Components */
 import Menu from "../../components/Menu/Menu";
 import AuthProvider from "../../components/AuthProvider/AuthProvider";
+import Notification from "../../components/ToastNotification/Notification";
 
 /* Util functions and constant values */
-import { LOGS_EDIT_URL_CONFIG, LOGS_LIST_URL_CONFIG, LOGS_LOGIN_URL_CONFIG, LOGS_NEW_URL_CONFIG } from "../../utils/constants";
+import { URL_CONFIG, LOGS_EDIT_URL_CONFIG, LOGS_LIST_URL_CONFIG, LOGS_LOGIN_URL_CONFIG, LOGS_NEW_URL_CONFIG } from "../../utils/constants";
+import { AppContext } from "../../store/Store";
+
+const appPages: AppPage[] = [
+  {
+    title: URL_CONFIG.LOGS_LOGIN.name,
+    url: URL_CONFIG.LOGS_LOGIN.path,
+    icon: contact
+  },
+  {
+    title: URL_CONFIG.LOGS_NEW.name,
+    url: URL_CONFIG.LOGS_NEW.path,
+    icon: timer
+  },
+  {
+    title: URL_CONFIG.LOGS_LIST.name,
+    url: URL_CONFIG.LOGS_LIST.path,
+    icon: list
+  }
+];
 
 const App: React.FC = () => {
+  const currentUser = getCurrentUser();
+
+  const { state, dispatch } = useContext(AppContext);
+  const {showNotification} = state;
+  
+  const onSuccessGetHours = (res: ILogs[]) => {
+    dispatch({
+      type: "UPDATE_LIST",
+      payload: res
+    });
+    dispatch({
+      type: "UPDATE_LOADING",
+      payload: false
+    });
+  };
+
+  const onErrorGetHours = () => {
+    dispatch({
+      type: "UPDATE_ERROR",
+      payload: true
+    });
+    dispatch({
+      type: "UPDATE_LOADING",
+      payload: false
+    });
+  };
+
+  useEffect(() => {
+    dispatch({
+      type: "UPDATE_LOADING",
+      payload: true
+    });
+    getHours(currentUser.id, onSuccessGetHours, onErrorGetHours);
+  }, []);
+  
   return (
     <IonApp>
       <IonReactRouter>
@@ -68,6 +126,19 @@ const App: React.FC = () => {
           </IonRouterOutlet>
         </IonSplitPane>
       </IonReactRouter>
+      {
+        showNotification &&
+        <Notification
+          message={state.notificationOptions.message}
+          duration={state.notificationOptions.duration}
+          color={state.notificationOptions.color}
+          header={state.notificationOptions.header}
+          showCloseButton={state.notificationOptions.showCloseButton}
+          closeButtonText={state.notificationOptions.closeButtonText}
+          position={state.notificationOptions.position}
+          isOpen={true}
+        />
+      }
     </IonApp>
   );
 };
