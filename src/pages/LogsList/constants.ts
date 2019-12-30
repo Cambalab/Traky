940b343afff2200076364069596dd16c0ce297b5
leonardo <leonardo@camba.lan>
2019-12-30 10:42:54 -0300

    Added Fab Button to log a new hour

--- src/pages/LogsList/constants.ts
+++ src/pages/LogsList/constants.ts
@@ -1,13 +1,29 @@
+import { LOGS_NEW_URL_CONFIG } from "../../utils/constants";
+import { alarm } from "ionicons/icons";
+
 export interface LogsListTexts {
     LIST_NO_LOGS_YET_MSG: string;
     LIST_TITLE: string;
     LIST_ERROR_MSG: string;
     BUTTON_NO_LOGS_YET_MSG: string;
 }
+
+export interface FabButtonOptions {
+    title: string;
+    url: string;
+    icon: object;
+}
+
 export const TEXTS: LogsListTexts = {
     BUTTON_NO_LOGS_YET_MSG: "OK, start now!",
     LIST_NO_LOGS_YET_MSG: "You dont have any log yet..",
     LIST_ERROR_MSG:
         "An error has occurred while triying to get the logged hours.",
     LIST_TITLE: "My logged hours"
+};
+
+export const NEW_HOUR_BUTTON_OPTION: FabButtonOptions = {
+    title: LOGS_NEW_URL_CONFIG.name,
+    url: LOGS_NEW_URL_CONFIG.path,
+    icon: alarm,
 };
