27c7a1c4e8836c3d512f020c71df56c97cae38a9
leonardo <leonardo@camba.lan>
2020-01-21 12:44:25 -0300

    Added loading modal for give feedback to user when the tryton key is being generate

--- src/pages/LoginSettings/LoginSettingsPage.tsx
+++ src/pages/LoginSettings/LoginSettingsPage.tsx
@@ -1,33 +1,42 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useState, useEffect } from "react";
 import {
   useAppContext
 } from "../../store/Store";
 import { History } from "history";
 import LoginSettingsForm from "../../components/LoginSettingsForm/LoginSettingsForm";
 import "./LoginSettingsPage.css";
-import { IonPage, IonContent, useIonViewDidEnter } from "@ionic/react";
+import { IonPage, IonContent, IonLoading, useIonViewDidEnter } from "@ionic/react";
 
 import {
   createErrorLoginSettingsAction,
   createSaveLoginSettingsAction,
   createSetKeyAction
 } from "./constants";
-import { KEY_INSTRUCTIONS_URL_CONFIG } from "../../utils/constants";
+import { KEY_INSTRUCTIONS_URL_CONFIG, GENERATE_KEY_MESSAGE } from "../../utils/constants";
 import { getUserAppKey } from "../../utils/api";
 import { ILoginSettings } from "../../utils/declarations";
 import {getStoredKey, getStoredSettings, storeKey, storeSettings} from "../../utils/utils";
 
 interface LoginSettingsPageProps {
   history: History;
+  initialShowLoading?: boolean
 }
 
 const LoginSettingsPage: FunctionComponent<LoginSettingsPageProps> = ({
-  history
+  history,
+  initialShowLoading = false
 }) => {
   const { state, dispatch } = useAppContext();
   const { settings } = state;
 
+  const [showLoading, setShowLoading] = useState(initialShowLoading);
+
+  useEffect(() => {
+    setShowLoading(initialShowLoading);
+  }, [initialShowLoading]);
+
   const onClickSave = async (body: ILoginSettings) => {
+    setShowLoading(true);
     const onSuccess = async (generatedKey: string) => {
       await storeSettings(body);
       await storeKey(generatedKey);
@@ -71,11 +80,21 @@ const LoginSettingsPage: FunctionComponent<LoginSettingsPageProps> = ({
   return (
     <IonPage>
       <IonContent color="tertiary">
+      {
+        showLoading &&
+        <IonLoading
+        isOpen={showLoading}
+        message={GENERATE_KEY_MESSAGE}
+        duration={2500}
+        onDidDismiss={() => setShowLoading(false)}
+        />
+      }
         <LoginSettingsForm
           onClickSave={onClickSave}
           initialServerAddress={settings.serverAddress}
           initialDatabase={settings.database}
           initialUsername={settings.username}
+          showLoading={showLoading}
         />
       </IonContent>
     </IonPage>
